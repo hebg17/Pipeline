b2947f6410c6fe47876b92c3864ae27a6d8bd153
Hugo Bravo <eickobravo@hotmail.com>
2022-04-28 18:54:55 -0500

    Agregado de Scripts para BD
    Se agregan los scripts necesarios para crear la estructura de BD

---
+++ BD/CreateProcedure_insertadatos.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE PROCEDURE MetrobusCDMX.insertadatos(
+	IN p_id integer,
+	IN pid numeric,
+	IN pdate_updated timestamp without time zone,
+	IN pvehicle_id numeric,
+	IN pvehicle_label numeric,
+	IN pvehicle_current_status numeric,
+	IN pposition_latitude numeric,
+	IN pposition_longitude numeric,
+	IN pgeographic_point text,
+	IN pposition_speed numeric,
+	IN pposition_odometer numeric,
+	IN ptrip_schedule_relationship numeric,
+	IN ptrip_id numeric,
+	IN ptrip_start_date numeric,
+	IN ptrip_route_id numeric)
+LANGUAGE 'sql'
+AS $BODY$
+	INSERT INTO MetrobusCDMX.unidades (_id, id, date_updated, vehicle_id, vehicle_label, vehicle_current_status, position_latitude, position_longitude, geographic_point, position_speed, position_odometer, trip_schedule_relationship, trip_id, trip_start_date, trip_route_id) VALUES (p_id, pid, pDate_updated, pVehicle_id, pVehicle_label, pVehicle_current_status, pPosition_latitude, pPosition_longitude, pGeographic_point, pPosition_speed, pPosition_odometer, pTrip_schedule_relationship, pTrip_id, pTrip_start_date, pTrip_route_id);
+$BODY$;
+ALTER PROCEDURE metrobuscdmx.insertadatos(integer, numeric, timestamp without time zone, numeric, numeric, numeric, numeric, numeric, text, numeric, numeric, numeric, numeric, numeric, numeric)
+    OWNER TO postgres;
---
+++ BD/CreateTables.sql
@@ -0,0 +1,31 @@
+CREATE TABLE MetrobusCDMX.alcaldias
+(
+    _id integer,
+    id numeric,
+    nomgeo text,
+    cve_mun numeric,
+    cve_ent numeric,
+    cvegeo numeric,
+    geo_point_2d text,
+    geo_shape text,
+    municipio numeric
+);
+
+CREATE TABLE MetrobusCDMX.unidades
+(
+    _id integer,
+    id numeric,
+    date_updated timestamp without time zone,
+    vehicle_id numeric,
+    vehicle_label numeric,
+    vehicle_current_status numeric,
+    position_latitude numeric,
+    position_longitude numeric,
+    geographic_point text COLLATE pg_catalog."default",
+    position_speed numeric,
+    position_odometer numeric,
+    trip_schedule_relationship numeric,
+    trip_id numeric,
+    trip_start_date numeric,
+    trip_route_id numeric
+);
